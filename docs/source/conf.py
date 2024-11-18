8d5e746fd3c9ccae06844599e607ac1da10f3d10
ziheng <ziheng0@163.com>
2024-11-18 16:51:07 +0800

    v0.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 project = 'QuecOpen'
 copyright = '2024, admin'
 author = 'admin'
-release = '0.3'
+release = '0.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
